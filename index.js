b38367a9da39fca551cd812a6d4e882f1df964b8
FrankNZYMT <frank.adeleye@nzymtechnologies.com>
2023-11-10 19:45:25 +0100

    feat: fixed QA review bugs

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 
 function ToggleSidebar() {
+	// we retrieve the element with specific className of 'mobile-nav' and save it in variable x
 	let x = document.querySelector(".mobile-nav");
 	
+	//on calling of this function, we check if the element is displayed 'flex', and then change it to 'none' or vise-versa
 	if (x.style.display === "flex") {
 		x.style.display = "none";
 	} else {
 		x.style.display = "flex";
 	}
 }
-
